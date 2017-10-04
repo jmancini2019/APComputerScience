db3e8d28b5b953dc328ddd6ded5c015c162f4091
jmancini2019 <jmancini2019@LAB-206-07.central.cchs>
2017-10-04 10:33:13 -0400

    COMMIT

--- Porblem2/Porblem2Part3.java
+++ Porblem2/Porblem2Part3.java
@@ -1,9 +1,11 @@
-
+//Julian Mancini
+//9/21/2017
+//displays text
 public class Porblem2Part3 {
-
+// I posted this to git
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("A well-formed Java program has \n a main method with { and } braces.");
 	}
 
 }
